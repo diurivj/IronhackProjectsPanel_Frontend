190df60e180af3c4d67590341f0f1d0f766b15ab
DeividZavala <crusedmoss@gmail.com>
2018-09-14 19:37:17 -0500

    defaul value en input

--- src/components/Student/Project1/Project1Container.js
+++ src/components/Student/Project1/Project1Container.js
@@ -33,28 +33,28 @@ class Project1Container extends Component{
       <div style={{display:'flex', alignItems:'center', justifyContent:'center', width:'100vw', height:'100vh', flexGrow: '1', flexWrap:'wrap'}}>
         <div style={{width:'100%', margin: '5px 1%', height:'15%' }}>
           <Card title='Title of your project'>
-            <Input type='text' placeholder='Title' name='title' onChange={this.handleChange} value={project.title}/>
+            <Input type='text' placeholder='Title' name='title' onChange={this.handleChange} defaultValue={project.title}/>
           </Card>
         </div>
         <div style={{width: '30%', margin: '1%' }}>
           <Card hoverable={true} title='Slides'>
             <Icon style={{color:'black',fontSize:'5rem'}} type="robot" theme="outlined" />
             <Divider/>
-              <Input type='url' name='presentation_slides' placeholder="Slides URL" onChange={this.handleChange} value={project.presentation_slides}/>
+              <Input type='url' name='presentation_slides' placeholder="Slides URL" onChange={this.handleChange} defaultValue={project.presentation_slides}/>
           </Card>
         </div>
         <div style={{width: '30%', margin: '1%'}}>
           <Card title='Deployed Site'>
             <Icon style={{color:'black',fontSize:'5rem'}} type="code" theme="outlined" />
             <Divider/>
-            <Input type='url' name='deployed_site' placeholder="Deployed Site URL" onChange={this.handleChange} value={project.deployed_site}/>
+            <Input type='url' name='deployed_site' placeholder="Deployed Site URL" onChange={this.handleChange} defaultValue={project.deployed_site}/>
           </Card>
         </div>
         <div style={{width: '30%', margin: '1%'}}>
           <Card title='Github'>
             <Icon style={{color:'black',fontSize:'5rem'}} type="github" theme="outlined" />
             <Divider/>
-            <Input type='url' name='github_repo' placeholder="Github URL" onChange={this.handleChange} value={project.github_repo}/>
+            <Input type='url' name='github_repo' placeholder="Github URL" onChange={this.handleChange} defaultValue={project.github_repo}/>
           </Card>
         </div>
           <Button style={{bottom:'5%'}} type='primary' onClick={this.handleSubmit}>Save</Button>
